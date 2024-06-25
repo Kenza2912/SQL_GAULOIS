be80f628cf493e704d7e562dd08bb03201eb47f6
Kenza <a.kenza29@gmail.com>
2024-06-25 17:51:59 +0200

    suite exo

--- index.sql
+++ index.sql
@@ -42,3 +42,50 @@ GROUP BY p.nom_potion
 ORDER BY coutRealisation DESC;
 
 -- 7. Nom des ingrédients + coût + quantité de chaque ingrédient qui composent la potion 'Santé'. 
+SELECT i.nom_ingredient, i.cout_ingredient, c.qte
+FROM ingredient i
+LEFT JOIN composer c ON i.id_ingredient = c.id_ingredient
+LEFT JOIN potion p ON c.id_potion = p.id_potion
+WHERE p.nom_potion = 'Santé';
+
+-- 8. Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'. 
+
+SELECT p.nom_personnage
+FROM prendre_casque pc
+JOIN personnage p ON pc.id_personnage = p.id_personnage
+JOIN bataille b ON pc.id_bataille = b.id_bataille
+WHERE b.nom_bataille ='Bataille du village gaulois'
+AND pc.qte = ( SELECT MAX(pc.qte) FROM prendre_casque pc); 
+
+
+-- 9. Nom des personnages et leur quantité de potion bue (en les classant du plus grand buveur au plus petit). 
+
+SELECT p.nom_personnage, SUM(b.dose_boire) AS quantiteBue
+FROM boire b 
+JOIN personnage p ON b.id_personnage = p.id_personnage
+GROUP BY p.nom_personnage
+ORDER BY quantiteBue DESC;
+
+-- 10. Nom de la bataille où le nombre de casques pris a été le plus important. 
+
+
+
+
+-- 11. Combien existe-t-il de casques de chaque type et quel est leur coût total ? (classés par nombre décroissant)
+
+SELECT tc.nom_type_casque, 
+COUNT(c.id_casque) AS nbCasques,
+SUM(c.cout_casque) AS coutTotal 
+FROM type_casque tc
+JOIN casque c ON tc.id_type_casque = c.id_type_casque
+GROUP BY  tc.nom_type_casque
+ORDER BY coutTotal DESC;
+
+
+
+-- 12. Nom des potions dont un des ingrédients est le poisson frais. 
+
+
+
+
+-- 13. Nom du / des lieu(x) possédant le plus d'habitants, en dehors du village gaulois. 
