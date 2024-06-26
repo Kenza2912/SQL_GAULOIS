ec2268813b4fd774399593790c22c7910dbdb9e6
Kenza <a.kenza29@gmail.com>
2024-06-26 11:59:19 +0200

    suite exo

--- index.sql
+++ index.sql
@@ -50,12 +50,37 @@ WHERE p.nom_potion = 'Santé';
 
 -- 8. Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'. 
 
-SELECT p.nom_personnage
-FROM prendre_casque pc
-JOIN personnage p ON pc.id_personnage = p.id_personnage
-JOIN bataille b ON pc.id_bataille = b.id_bataille
-WHERE b.nom_bataille ='Bataille du village gaulois'
-AND pc.qte = ( SELECT MAX(pc.qte) FROM prendre_casque pc); 
+-- SELECT p.nom_personnage
+-- FROM prendre_casque pc
+-- JOIN personnage p ON pc.id_personnage = p.id_personnage
+-- JOIN bataille b ON pc.id_bataille = b.id_bataille
+-- WHERE b.nom_bataille ='Bataille du village gaulois'
+-- AND pc.qte = ( SELECT MAX(pc.qte) FROM prendre_casque pc); 
+
+
+-- SELECT p.nom_personnage, pc.qte
+-- FROM prendre_casque pc
+-- JOIN personnage p ON pc.id_personnage = p.id_personnage
+-- JOIN bataille b ON pc.id_bataille = b.id_bataille
+-- WHERE b.nom_bataille ='Bataille du village gaulois'
+-- AND pc.qte = ( SELECT SUM(pc.qte) FROM prendre_casque pc 
+-- WHERE pc.qte >= ALL (SELECT pc.qte FROM prendre_casque pc 
+-- JOIN bataille b ON pc.id_bataille = b.id_bataille 
+-- WHERE b.nom_bataille ='Bataille du village gaulois' ));
+
+SELECT p.nom_personnage, SUM(pc.qte) AS nb_casques
+FROM personnage p, bataille b, prendre_casque pc
+WHERE p.id_personnage = pc.id_personnage
+AND pc.id_bataille = b.id_bataille
+AND b.nom_bataille = 'Bataille du village gaulois'
+GROUP BY p.id_personnage
+HAVING nb_casques >= ALL(
+SELECT SUM(pc.qte)
+            FROM prendre_casque pc, bataille b
+            WHERE b.id_bataille = pc.id_bataille
+            AND b.nom_bataille = 'Bataille du village gaulois'
+            GROUP BY pc.id_personnage
+)
 
 
 -- 9. Nom des personnages et leur quantité de potion bue (en les classant du plus grand buveur au plus petit). 
@@ -67,6 +92,14 @@ GROUP BY p.nom_personnage
 ORDER BY quantiteBue DESC;
 
 -- 10. Nom de la bataille où le nombre de casques pris a été le plus important. 
+SELECT b.nom_bataille, SUM( pc.qte) AS nbCasques
+FROM bataille b
+JOIN prendre_casque pc ON b.id_bataille = pc.id_bataille
+GROUP BY b.id_bataille, b.nom_bataille
+HAVING SUM(pc.qte) >= ALL(
+	SELECT SUM(pc.qte)
+	FROM prendre_casque pc
+	GROUP BY pc.id_bataille);
 
 
 
@@ -85,7 +118,80 @@ ORDER BY coutTotal DESC;
 
 -- 12. Nom des potions dont un des ingrédients est le poisson frais. 
 
+SELECT DISTINCT p.nom_potion
+FROM composer c
+JOIN potion p ON c.id_potion = p.id_potion
+JOIN ingredient i ON c.id_ingredient = i.id_ingredient
+WHERE i.nom_ingredient = 'Poisson frais';
+
 
 
 
 -- 13. Nom du / des lieu(x) possédant le plus d'habitants, en dehors du village gaulois. 
+
+SELECT l.nom_lieu, SUM(p.id_personnage) AS nbHabitants
+FROM lieu l
+INNER JOIN personnage p ON l.id_lieu = p.id_lieu
+WHERE l.nom_lieu != 'Village gaulois'
+GROUP BY l.nom_lieu
+HAVING nbHabitants >= ALL (
+SELECT SUM(p.id_personnage)
+					FROM lieu l
+					INNER JOIN personnage p ON l.id_lieu = p.id_lieu
+					WHERE l.nom_lieu != 'Village gaulois'
+					GROUP BY l.nom_lieu);
+
+
+
+-- 14. Nom des personnages qui n'ont jamais bu aucune potion.
+
+SELECT p.nom_personnage
+FROM personnage p
+WHERE p.id_personnage NOT IN(
+				SELECT DISTINCT b.id_personnage
+				FROM boire b);
+
+
+
+-- 15. Nom du / des personnages qui n'ont pas le droit de boire de la potion 'Magique'.
+SELECT p.nom_personnage
+FROM personnage p
+WHERE p.id_personnage NOT IN (
+					SELECT DISTINCT ab.id_personnage
+					FROM autoriser_boire ab
+					WHERE ab.id_potion =(
+					SELECT DISTINCT po.id_potion
+					FROM potion po
+					WHERE po.nom_potion = 'Magique'));
+
+
+
+-- A. Ajoutez le personnage suivant : Champdeblix, agriculteur résidant à la ferme Hantassion de Rotomagus.
+
+
+
+
+
+
+-- B. Autorisez Bonemine à boire de la potion magique, elle est jalouse d'Iélosubmarine...
+
+
+
+
+
+-- C. Supprimez les casques grecs qui n'ont jamais été pris lors d'une bataille.
+
+
+
+
+-- D. Modifiez l'adresse de Zérozérosix : il a été mis en prison à Condate
+
+
+
+
+-- E. La potion 'Soupe' ne doit plus contenir de persil.
+
+
+
+
+-- F. Obélix s'est trompé : ce sont 42 casques Weisenau, et non Ostrogoths, qu'il a pris lors de la bataille 'Attaque de la banque postale'. Corrigez son erreur !
